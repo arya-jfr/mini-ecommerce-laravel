16faf4d648862825348fbf86e5d075a44c725bfc
arya-jfr <arya.jafari.2575@gmail.com>
2026-02-21 19:22:35 +0330

    add assets, implement view layouts

---
+++ app/Models/Admin.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Admin
+ * 
+ * @property int $id
+ * @property string $full_name
+ * @property string $username
+ * @property string $password
+ * @property int $status
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @package App\Models
+ */
+class Admin extends Model
+{
+	protected $table = 'admins';
+	public static $snakeAttributes = false;
+
+	protected $casts = [
+		'status' => 'int'
+	];
+
+	protected $hidden = [
+		'password'
+	];
+
+	protected $fillable = [
+		'full_name',
+		'username',
+		'password',
+		'status'
+	];
+}
