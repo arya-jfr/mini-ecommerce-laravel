2c460a0fdaa0ad49e25c2522322722a6727a78ce
arya-jfr <arya.jafari.2575@gmail.com>
2026-02-22 10:20:11 +0330

    bug fixes

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -350,7 +350,7 @@
                                 </div>
                             </div>
                             <!-- badge offer -->
-                            <span class="product-card_badge">70% تخفیف‌</س>
+                            <span class="product-card_badge">70% تخفیف‌</span>
                         </div>
                         <!-- product img -->
                         <a href="product-details.html">
@@ -426,7 +426,7 @@
                                 </div>
                             </div>
                             <!-- badge offer -->
-                            <span class="product-card_badge">70% تخفیف‌</س>
+                            <span class="product-card_badge">70% تخفیف‌</span>
                         </div>
                         <!-- product img -->
                         <a href="product-details.html">
@@ -502,7 +502,7 @@
                                 </div>
                             </div>
                             <!-- badge offer -->
-                            <span class="product-card_badge">70% تخفیف‌</س>
+                            <span class="product-card_badge">70% تخفیف‌</span>
                         </div>
                         <!-- product img -->
                         <a href="product-details.html">
@@ -654,7 +654,7 @@
                                 </div>
                             </div>
                             <!-- badge offer -->
-                            <span class="product-card_badge">70% تخفیف‌</س>
+                            <span class="product-card_badge">70% تخفیف‌</span>
                         </div>
                         <!-- product img -->
                         <a href="product-details.html">
@@ -730,7 +730,7 @@
                                 </div>
                             </div>
                             <!-- badge offer -->
-                            <span class="product-card_badge">70% تخفیف‌</س>
+                            <span class="product-card_badge">70% تخفیف‌</span>
                         </div>
                         <!-- product img -->
                         <a href="product-details.html">
